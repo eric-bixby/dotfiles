d38ed83f8bb752539ff6a3598bd0bf787f3ba087
Eric Bixby <ebixby@yahoo.com>
2023-04-22 20:30:59 -0700

    Add scripts to restore bookmarks

---
+++ bookmarks/restore_brave_bookmarks.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp Bookmarks-brave.json "$HOME/Library/Application Support/BraveSoftware/Brave-Browser/Default/Bookmarks"
+
---
+++ bookmarks/restore_edge_bookmarks.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp Bookmarks-edge.json "$HOME/Library/Application Support/Microsoft Edge/Default/Bookmarks"
+
