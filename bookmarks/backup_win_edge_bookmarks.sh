45640dfa1cfb385223cdd82f4dcfab9fe3af19ee
Eric Bixby <ebixby@yahoo.com>
2023-04-22 20:48:55 -0700

    Backup/restore Win Edge bookmarks

---
+++ bookmarks/backup_win_edge_bookmarks.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp "$LOCALAPPDATA/Microsoft/Edge/User Data/Default/Bookmarks" Bookmarks-win-edge.json
+
---
+++ bookmarks/restore_win_edge_bookmarks.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp Bookmarks-win-edge.json "$LOCALAPPDATA/Microsoft/Edge/User Data/Default/Bookmarks"
+
