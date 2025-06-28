16411883316af829648275d455d7e28fa8b2f387
Eric Bixby <ebixby@yahoo.com>
2025-06-28 12:46:14 -0700

    Add new scripts

---
+++ git/bin/mount_docs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo mount -t cifs //nas-ba-f3-f8.local/documents/ -o username='eric',domain='workgroup' /mnt/documents
+
---
+++ git/bin/update_parse.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mv ~/Downloads/parse.py .
+chmod +x parse.py
+./parse.py
+
