15ea8ad921af49201ba4b778401b5c6d3f35f34b
Eric Bixby <ebixby@yahoo.com>
2025-06-28 17:48:02 -0700

    Add uid and gid to mount

--- git/bin/mount_docs.sh
+++ git/bin/mount_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo mount -t cifs //nas-ba-f3-f8.local/documents/ -o username='eric',domain='workgroup' /mnt/documents
+sudo mount -t cifs //nas-ba-f3-f8.local/documents/ -o username='eric',domain='workgroup',uid=$(id -u),gid=$(id -g) /mnt/documents
 
