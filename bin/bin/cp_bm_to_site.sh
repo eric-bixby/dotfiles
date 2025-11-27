7c23e5b4ccd5f12ef5037b0efc31bb520c44b3e7
Eric Bixby <ebixby@yahoo.com>
2025-11-27 06:52:30 -0800

    Remove copy script

--- bin/bin/cp_bm_to_site.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp ~/git/bookmarks/index.html ~/git/eric-bixby.github.io/bookmarks.html
-cd ~/git/eric-bixby.github.io && git add bookmarks.html && git commit -m "Update bookmarks" && git push
-
